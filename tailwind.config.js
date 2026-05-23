c5a8072547310a73d68dfb17f474fd7264d91e83
swadeshchisim <schisim195@gmail.com>
2026-05-23 22:11:38 +0600

    container added

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        xl: '1400px',
+      },
+    },
+  },
+}
